cf23a75dbac2f6c4a19f30fb49c8068282151ccf
Itay Oshri <itay.oshri@gmail.com>
2022-07-13 00:58:37 +0300

    FIX

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,8 +5,6 @@ export interface InputProps {
 export default function Input({ onChange }: InputProps) {
   return (
     <input
-      pattern="[0-9]*"
-      inputMode="numeric"
       type={'number'}
       onChange={(e) => onChange(Number(e.target.value))}
       className=" appearance-none bg-white w-40 h-12 text-right rounded-lg border-2 px-3 border-transparent outline-none shadow-sm shadow-black/30"
