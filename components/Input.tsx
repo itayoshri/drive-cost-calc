0bc10aa3f0688d182545be867c0c6ee92b0e2fd9
Itay Oshri <itay.oshri@gmail.com>
2022-07-14 00:36:46 +0300

    FEATURE: Hint when not selected

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,15 +1,21 @@
+import { useState } from 'react'
+
 export interface InputProps {
   onChange(input: number): unknown
   hint: string
 }
 
 export default function Input({ onChange, hint }: InputProps) {
+  const [selected, setSelected] = useState(false)
+
   return (
     <input
       type={'number'}
-      placeholder={hint}
       onChange={(e) => onChange(Number(e.target.value))}
       className=" appearance-none bg-white w-40 h-12 text-right rounded-lg border-2 px-3 border-transparent outline-none shadow-sm shadow-black/30"
+      placeholder={selected ? '' : hint}
+      onSelect={() => setSelected(true)}
+      onBlur={() => setSelected(false)}
     />
   )
 }
